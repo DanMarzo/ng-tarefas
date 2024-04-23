c3d2325c732bc6cb48d3163cf8d65851b48cdc86
DanMarzo <marzogildan@gmail.com>
2024-04-22 22:14:54 -0300

    Colocando components

--- src/app/modules/todo-list/page/list/list.component.ts
+++ src/app/modules/todo-list/page/list/list.component.ts
@@ -1,10 +1,11 @@
 import { NgIf } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf,InputAddItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
