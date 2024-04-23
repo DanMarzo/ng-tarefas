b679661644e8e43d66afcf7502074b71fafddbaf
DanMarzo <marzogildan@gmail.com>
2024-04-22 23:09:35 -0300

    Usando outputs cabulosos

--- src/app/modules/todo-list/components/input-add-item/input-add-item.component.ts
+++ src/app/modules/todo-list/components/input-add-item/input-add-item.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  inject,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { IListItems } from '../../interface/IListitems.interface';
 
 @Component({
   selector: 'app-input-add-item',
   standalone: true,
   imports: [],
   templateUrl: './input-add-item.component.html',
-  styleUrl: './input-add-item.component.scss'
+  styleUrl: './input-add-item.component.scss',
 })
 export class InputAddItemComponent {
+  #cdr = inject(ChangeDetectorRef);
+//ele pega o child com essa variavel do child HTML
+  @ViewChild('inputText') public inputText!: ElementRef;
+  @Output() public outPutListItems = new EventEmitter<IListItems>();
 
+  public focusAndAddItem(value: string) {
+    if (value) {
+      this.#cdr.detectChanges();
+      this.inputText.nativeElement.value = "";
+
+      const dataAtual = new Date();
+      const timeStamp = dataAtual.getTime();
+      const id = `ID ${timeStamp}`
+
+      this.outPutListItems.emit({
+        id,
+        checked:false,
+        value
+      })
+      return this.inputText.nativeElement.focus();
+    }
+  }
 }
