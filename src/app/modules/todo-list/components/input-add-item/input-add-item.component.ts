1fdc120a5e29074df30302774cc854342fe3cacd
DanMarzo <marzogildan@gmail.com>
2024-05-01 20:14:40 -0300

    Inclusao de INput

--- src/app/modules/todo-list/components/input-add-item/input-add-item.component.ts
+++ src/app/modules/todo-list/components/input-add-item/input-add-item.component.ts
@@ -4,32 +4,35 @@ import {
   ElementRef,
   EventEmitter,
   inject,
+  Input,
   Output,
   ViewChild,
 } from '@angular/core';
 import { IListItems } from '../../interface/IListitems.interface';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-input-add-item',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './input-add-item.component.html',
   styleUrl: './input-add-item.component.scss',
 })
 export class InputAddItemComponent {
   #cdr = inject(ChangeDetectorRef);
-//ele pega o child com essa variavel do child HTML
+  //ele pega o child com essa variavel do child HTML
   @ViewChild('inputText') public inputText!: ElementRef;
   /**
    * O @output e eventemitr quer dizer que a aquele component esta enviando determinada informacao
    */
+  @Input({required: true}) public inputListItems: Array<IListItems> = [];
   @Output() public oututAddListItems = new EventEmitter<IListItems>();
+
 
   public focusAndAddItem(value: string) {
     if (value) {
       this.#cdr.detectChanges();
       this.inputText.nativeElement.value = "";
-
       const dataAtual = new Date();
       const timeStamp = dataAtual.getTime();
       const id = `ID ${timeStamp}`
